4f43fa249dc7d8db5c77441659aaabdc3981bd06
Daniel <daniel21dev@gmail.com>
2021-11-26 23:04:05 -0600

    todos delete testing

--- test/todos.test.js
+++ test/todos.test.js
@@ -4,11 +4,14 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const bcrypt = require('bcryptjs')
 const { generateJwt } = require('../src/helpers/generteJwt')
+const { addMinutes } = require('date-fns')
 
 describe('/todos', () => {
     // declares a testing user in order to test the login
     let testingUser;
     let token;
+    let testingTodo;
+
     beforeAll(async () => {
         const salt = bcrypt.genSaltSync(10);
         const securePassword = bcrypt.hashSync('123456', salt);
@@ -52,6 +55,55 @@ describe('/todos', () => {
         expect(res.body.todo.title  ).toBe(data.title)
         expect(res.body.todo.content).toBe(data.content)
         expect(res.body.todo.dueDate).toBe(data.dueDate)
+        expect(res.body.todo.active).toBe(true)
+        expect(res.body.todo.userId).toBe(testingUser.id)
+        testingTodo = res.body.todo
+    })
+
+    it('should update a todo', async () => {
+        const data = {
+            title: 'todo title updated',
+            content: "i dont know updated",
+            dueDate: addMinutes(new Date(),30).toISOString(),
+            completed: true
+        }
+        const res = await request(app)
+            .put(`/api/v1/todos/${testingTodo.id}`)
+            .set('x-token', token)
+            .send(data)
+        expect(res.status).toBe(200)
+        expect(res.body.todo.title  ).toBe(data.title)
+        expect(res.body.todo.content).toBe(data.content)
+        expect(res.body.todo.dueDate).toBe(data.dueDate)
+        expect(res.body.todo.completed).toBe(data.completed)
+        expect(res.body.todo.active).toBe(true)
+        expect(res.body.todo.userId).toBe(testingUser.id)
+    })
+
+    it('should return one todo', async () => {
+        const res = await request(app)
+            .get('/api/v1/todos')
+            .set('x-token', token)
+            .send()
+        expect(res.status).toBe(200)
+        expect(res.body.todos.length).toBe(1)
+    })
+
+    it('should delete a todo', async () => {
+        const res = await request(app)
+            .delete(`/api/v1/todos/${testingTodo.id}`)
+            .set('x-token', token)
+            .send()
+        expect(res.status).toBe(200)
+    })
+
+    it('should respond with an empty array (again)', async () => {
+        const res = await request(app)
+            .get('/api/v1/todos')
+            .set('x-token', token)
+            .send()
+        expect(res.status).toBe(200)
+        expect(res.body.todos.length).toBe(0)
     })
 
     // deletes the testing user

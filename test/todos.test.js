e24e0de5d26d391b1b9770151b0f3d09eca441e2
Daniel <daniel21dev@gmail.com>
2021-11-26 22:53:12 -0600

    solves ondelete todos problem and test create a todo

--- test/todos.test.js
+++ test/todos.test.js
@@ -14,21 +14,44 @@ describe('/todos', () => {
         const securePassword = bcrypt.hashSync('123456', salt);
         testingUser = await prisma.user.create({
             data: {
-                email: 'user3@testing.com',
+                email: 'user4@testing.com',
                 name: 'testinguser',
                 password: securePassword
             }
         })
-        token = await generateJwt(testingUser.id,'1d')
+        token = await generateJwt(testingUser.id, '1d')
+    })
+
+    it('should respond with 401 status', async () => {
+        const res = await request(app)
+            .get('/api/v1/todos')
+            .send()
+        expect(res.status).toBe(401)
     })
 
     it('should respond with an empty array', async () => {
         const res = await request(app)
             .get('/api/v1/todos')
-            .set('x-token',token)
+            .set('x-token', token)
             .send()
         expect(res.status).toBe(200)
         expect(res.body.todos.length).toBe(0)
+    })
+
+    it('should create a todo', async () => {
+        const data = {
+            title: 'todo title',
+            content: "i dont know",
+            dueDate: new Date().toISOString()
+        }
+        const res = await request(app)
+            .post('/api/v1/todos')
+            .set('x-token', token)
+            .send(data)
+        expect(res.status).toBe(200)
+        expect(res.body.todo.title  ).toBe(data.title)
+        expect(res.body.todo.content).toBe(data.content)
+        expect(res.body.todo.dueDate).toBe(data.dueDate)
     })
 
     // deletes the testing user

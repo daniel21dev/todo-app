07550711e3a50d2dee5b858b8785f111f6316e48
Daniel <daniel21dev@gmail.com>
2021-11-26 22:22:17 -0600

    login test

--- test/login.test.js
+++ test/login.test.js
@@ -2,16 +2,19 @@ const app = require('../src/app')
 const request = require('supertest')
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
+const bcrypt = require('bcryptjs')
 
 describe('POST /users', () => {
     // declares a testing user in order to test the login
     let testingUser;
     beforeAll(async () => {
+        const salt = bcrypt.genSaltSync(10);
+        const securePassword = bcrypt.hashSync('123456', salt);
         testingUser = await prisma.user.create({
             data: {
                 email: 'user@testing.com',
                 name: 'testinguser',
-                password: '123456'
+                password: securePassword
             }
         })
     })
@@ -27,6 +30,16 @@ describe('POST /users', () => {
             password: 'incorrectpassword'
         })
         expect(res.status).toBe(500);
+    })
+
+    it('should respond with 200 status code and resturn a token', async () => {
+        const res = await request(app).post('/api/v1/login').send({
+            email: 'user@testing.com',
+            password: '123456'
+        })
+        expect(res.status).toBe(200);
+        expect(res.body.token).not.toBe(null)
+        expect(res.body.token).not.toBe(undefined)
     })
 
     // deletes the testing user
